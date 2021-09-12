d1a61d71ee4e21c5b1806e5311c16595ba7bad45
Serbatico <Serbatico@yandex.ru>
2021-09-12 18:17:45 +0300

    -a

--- src/MakeRubFor100.java
+++ src/MakeRubFor100.java
@@ -2,20 +2,27 @@ public class MakeRubFor100 {
 
     public static void main(String[] args) {
         //,,,,,,,,,,,,,,,,,,Пополняем, не стесняемся!,,,,,,,,,,,,,,,,,,,,,,//
-        int balance = 143; //Баланс до пополнения
-        int addMoney = 12321; // Сколько добавить ?
+        int balance = 100; //Баланс до пополнения
+        int addMoney = 1000; // Сколько добавить ?
         //.................................................................//
 
-        int bonus = addMoney / 100;
-        int balanceAfter = balance + addMoney + bonus;
+        int bonus = 0;
 
-        if (addMoney < 1000) {
-            System.out.println("Спасибо за пополнение Вашего счёта!");
-            System.out.println("На вашем счету: " +balanceAfter);
-        } else {
-            System.out.println("Спасибо за пополнение Вашего счёта!");
-            System.out.println("На вашем счету: " + balanceAfter + " рублей");
-            System.out.println("Вам начисленно: "+ bonus +" бонус-рублей.");
+        if (addMoney >= 1000) {
+            bonus = addMoney / 100;
+
         }
+
+        balance += addMoney + bonus;
+        System.out.println("Итоговый счёт: " + balance + " рублей." );
+
+
+        if (bonus != 0)
+            System.out.println("Вам начислили: " + bonus + " бонусных рублей.");
+
+
+
+
+
     }
 }
